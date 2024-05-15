a5d4df9c5a397a6acad3916b680629cd42707c23
iLucruS <90245001+iLucruS@users.noreply.github.com>
2024-05-15 14:18:32 +0300

    Added realization

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import os
+from docx import Document
+from docx2txt import process
 
+
+def extract_images_from_docx(docx_path, output_dir):
+    if not os.path.exists(docx_path):
+        print(f"File {docx_path} doesn't exists.")
+        return
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    process(docx_path, output_dir)
+
+    print(f"All images was saved to {output_dir}")
+
+
+docx_path = 'file.docx'
+
+output_dir = 'images'
+
+extract_images_from_docx(docx_path, output_dir)
